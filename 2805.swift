93ea7fec9279ee4f428b10022f8805571318ad20
Neph <tony3322@naver.com>
2022-06-23 17:40:17 +0900

    2805 나무 자르기 문제 주석 추가

--- 2805.swift
+++ 2805.swift
@@ -1,6 +1,11 @@
 /*
  나무의 수(N)는 1e6 길이(M)는 2e9까지가 범위인 문제이다.
  이진탐색으로 O(NlogM)에 해결할 수 있을 것 같다.
+
+ 가끔 이진탐색시에 mid의 값이 (left + right) / 2 인지
+ 여기에 +1 한건지가 헷갈릴땐
+ left 30 right 31 같은 1 차이나는 상황일때 (30이 정답인 경우)
+ 를 만족해야함을 생각하자
  */
 var arr = [Int]()
 let nm = readLine()!.split(separator: " ").map { Int(String($0))! }

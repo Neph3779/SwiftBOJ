3ae2a5821db306695e90ce7dfbe396eec4757d49
Neph <tony3322@naver.com>
2022-06-23 17:36:42 +0900

    2805 나무 자르기 문제 풀이 업로드

---
+++ 2805.swift
@@ -0,0 +1,44 @@
+/*
+ 나무의 수(N)는 1e6 길이(M)는 2e9까지가 범위인 문제이다.
+ 이진탐색으로 O(NlogM)에 해결할 수 있을 것 같다.
+ */
+var arr = [Int]()
+let nm = readLine()!.split(separator: " ").map { Int(String($0))! }
+let m = nm[1]
+let treeHeights = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+
+
+func totalTreeLength(cutHeight: Int) -> Int {
+    var sum = 0
+    for height in treeHeights {
+        if height > cutHeight {
+            sum += (height - cutHeight)
+        }
+    }
+    return sum
+}
+
+var temp = 0
+var answer = 0
+var left = 0
+var right = 2000000000
+while true {
+    let mid = (left + right) / 2
+    let totalLength = totalTreeLength(cutHeight: mid)
+    if totalLength > m {
+        if temp == mid {
+            answer = mid
+            break
+        }
+        temp = mid
+        left = mid + 1
+    } else if totalLength == m {
+        answer = mid
+        break
+    } else {
+        right = mid - 1
+    }
+}
+
+print(answer)
